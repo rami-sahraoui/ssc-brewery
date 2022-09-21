0f6b0034b5ba5239620b2684310138ddbd228aad
ChefServiceInfo <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>
2022-09-21 14:15:35 +0100

    Spring Security Java Configuration

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        ((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)http
+                .authorizeRequests(authorize -> {
+                    authorize
+                            .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                            .antMatchers("/beers/find", "/beers*").permitAll()
+                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+                })
+                .authorizeRequests()
+                .anyRequest()).authenticated()
+                .and()).formLogin()
+                .and().httpBasic();
+    }
+}
