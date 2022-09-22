3c357c8e94e353d3c57696c81d613e82573a0c00
rami-sahraoui <97910592+rami-sahraoui@users.noreply.github.com>
2022-09-22 06:52:17 +0100

    Password Security

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +9,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//        return new LdapShaPasswordEncoder();
+//        return new StandardPasswordEncoder();
+//        return new BCryptPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         ((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)http
@@ -31,12 +50,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("spring")
-                .password("{noop}guru")
+                .password("{bcrypt}$2a$10$qWAYeQsZmd1aF2r.wjCJ.u/9A2kh3rDinaCSIgoyjXz8k2nIj9JmG")
+//                .password("guru")
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{noop}password")
+                .password("{sha256}0e342c1c6c9b7aa476df0946fa1dfd64f02bec13c846624b06c28d584ffc15cfd4829f8e1deec4db")
+//                .password("$2a$10$.9GwRr9luSZKDCY/bQG2.OGELZRnvWnZXK9Pitp1Kmjiu3Rax.Fxu")
+//                .password("caea0368f084ad303c14352a1ab3c81f362eb6aff15516124309a03e658cb4bcab3ce86f6ab47231")
+//                .password("{SSHA}BakcEiQm6Gw2KgMDlB9qSubr6BaHrfEJZ1kCnQ==")
+//                .password("password")
                 .roles("USER");
+
+        auth.inMemoryAuthentication()
+                .withUser("scott")
+                .password("{ldap}{SSHA}484hWH9aHb7LKxDMSheVHTQ8577GpszTJx1X0A==")
+//                .password("tiger")
+                .roles("CUSTOMER");
     }
 
     //    @Override
--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,10 +1,52 @@
 package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.util.DigestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordEncodingTest {
     static final String PASSWORD = "password";
+
+    @Test
+    void testBCrypt() {
+        PasswordEncoder bCrypt = new BCryptPasswordEncoder();
+
+        System.out.println(bCrypt.encode(PASSWORD));
+        System.out.println(bCrypt.encode(PASSWORD));
+        System.out.println(bCrypt.encode("guru"));
+    }
+
+    @Test
+    void testSha256() {
+        PasswordEncoder sha256 = new StandardPasswordEncoder();
+
+        System.out.println(sha256.encode(PASSWORD));
+        System.out.println(sha256.encode(PASSWORD));
+    }
+
+    @Test
+    void testLdap() {
+        PasswordEncoder ldap = new LdapShaPasswordEncoder();
+        System.out.println(ldap.encode(PASSWORD));
+        System.out.println(ldap.encode(PASSWORD));
+        System.out.println(ldap.encode("tiger"));
+
+        String encodedPwd = ldap.encode(PASSWORD);
+        assertTrue(ldap.matches(PASSWORD, encodedPwd));
+    }
+
+    @Test
+    void testNoOp() {
+        PasswordEncoder noOp = NoOpPasswordEncoder.getInstance();
+
+        System.out.println(noOp.encode(PASSWORD));
+    }
 
     @Test
     void hashingExample() {
