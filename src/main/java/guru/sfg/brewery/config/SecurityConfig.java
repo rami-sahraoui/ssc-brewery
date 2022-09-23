c5649ccec3a247001424ba6bc28320de7900c410
rami-sahraoui <97910592+rami-sahraoui@users.noreply.github.com>
2022-09-23 07:24:08 +0100

    Allow Multiple Role

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -43,7 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                             .antMatchers("/beers/find", "/beers*").permitAll()
                             .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+                            .antMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
+                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
+                            .mvcMatchers(HttpMethod.GET,"/brewery/breweries")
+                                .hasAnyRole("ADMIN","CUSTOMER")
+                            .mvcMatchers(HttpMethod.GET,"/brewery/api/v1/breweries")
+                                .hasAnyRole("ADMIN","CUSTOMER");
                 })
                 .authorizeRequests()
                 .anyRequest()).authenticated()
